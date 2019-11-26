9cbfeb4049e54a69d54bb27df113d439bb77609c
monchi <12588861+monchiestevez@users.noreply.github.com>
2019-11-25 19:50:12 -0500

    main

---
+++ main.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from SI1145.SI1145 as SI1145
+import time
+
+
+sensor = SI1145.SI1145()
+
+
+def main():
+    with open("logs.txt", "a") as myfile:
+        vis = sensor.readVisible()
+        IR = sensor.readIR()
+        UV = sensor.readUV()
+        uvIndex = UV / 100.0
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        myfile.write(dt_string + " Vis: " + str(vis) + " IR: " + str(IR) + " UV Index: " + str(uvIndex) + "\n")
+
+
+while True:
+    main()
+    time.sleep(5)
